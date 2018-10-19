4ce1bdfe8210263c478552f266cfe34bdd7d3fec
Cristian Leonardo Arango Daza <cristianarangodaza@gmail.com>
2018-10-19 15:33:09 -0500

    First

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require("./routes/api/post");
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("server start on port"));
