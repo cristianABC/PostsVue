6cf7615f6bb6a247b18c110f379a2799c9005682
Cristian Leonardo Arango Daza <cristianarangodaza@gmail.com>
2018-11-06 17:11:37 -0500

    Commit End

--- server/index.js
+++ server/index.js
@@ -10,5 +10,12 @@ app.use(cors());
 const posts = require("./routes/api/post");
 app.use("/api/posts", posts);
 
+//Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("server start on port"));
